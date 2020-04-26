3ecffa80511fc6b8f6c0392fb089111a9df60c8e
ZachGraceffa <zachgraceffa@gmail.com>
2020-04-26 13:53:59 -0500

    removed exfat related apps from focal fossa scripts due to native support

--- ubuntu_20/000-base.sh
+++ ubuntu_20/000-base.sh
@@ -9,7 +9,7 @@ sudo apt upgrade -y && sudo apt dist-upgrade -y
 sudo apt autoremove -y
 
 # Install some critical packages
-sudo apt install -y htop git dkms exfat-fuse exfat-utils jmtpfs linux-tools-generic msr-tools devmem2 apt-file
+sudo apt install -y htop git dkms jmtpfs linux-tools-generic msr-tools devmem2 apt-file
 
 # Set git configuration
 git config --global user.name $GIT_USERNAME
